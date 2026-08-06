618e7f7a5037b998314dc2db244429d0c48c061f
Gradi <gradipuata@gmail.com>
2026-08-06 20:26:16 +0200

    added routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
-const App = () => {
-  return <div></div>;
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Chat } from "./pages/Chat";
+import { Education } from "./pages/Education";
+import { Experience } from "./pages/Experience";
+import { Projects } from "./pages/Projects";
+import { NotFound } from "./pages/NotFound";
+const App: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <nav>
+        <Link to={"/chat"} />
+        <Link to={"/education"} />
+        <Link to={"/experience"} />
+        <Link to={"/projects"} />
+        <Link to={"/notfound"} />
+      </nav>
+
+      {/* Routes */}
+      <Routes>
+        <Route path="/chat" element={<Chat />} />
+        <Route path="/education" element={<Education />} />
+        <Route path="/experience" element={<Experience />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/notfound" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
